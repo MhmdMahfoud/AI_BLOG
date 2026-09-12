bec5d672d5a9cea54485a1070a51b833b268aa06
Mohammad Mahfoud <engmohammedmahfoud2001@gmail.com>
2026-09-12 17:22:24 +0300

    add context

--- Frontend/frontend/src/context/AuthContext.tsx
+++ Frontend/frontend/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 interface AuthContextType{
     user:User | null;
     loading:boolean;
@@ -8,4 +8,5 @@ logout:()=>Promise<void>
 checkAuth:()=>Promise<void>
 
 }
-const AuthContext=createContext<AuthContextType|undefined>
+const AuthContext=createContext<AuthContextType|undefined>(undefined)
+export const useAuth=():AuthContextType=>{const context=useContext(AuthContext)}
